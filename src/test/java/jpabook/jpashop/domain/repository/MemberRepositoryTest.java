ae704486817374518f4921890e3882da437ee7df
KKambi <bok03220@gmail.com>
2021-07-05 00:30:33 +0900

    test: member repository 엔티티 생성과 조회

---
+++ src/test/java/jpabook/jpashop/domain/repository/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("멤버 레포지토리")
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @DisplayName("저장 및 조회")
+    @Transactional
+    @Test 
+    public void testMember() {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member foundMember = memberRepository.find(savedId);
+
+        // then
+        assertThat(foundMember.getId()).isEqualTo(member.getId());
+        assertThat(foundMember.getUsername()).isEqualTo(member.getUsername());
+
+    }
+
+}
